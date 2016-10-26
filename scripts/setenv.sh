6b36e8b1d0786d94449ffcc79ebe18a5f841e8fc
Joe Rice <joericearchitect@gmail.com>
2016-10-25 21:54:37 -0400

    adding 'export' to all variable definitions so they will be available to docker-compose files

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,11 +1,11 @@
-SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
+export SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-INFRA_MODULES_HOME_DIR=$SCRIPTS_DIR/../infra-modules
+export INFRA_MODULES_HOME_DIR=$SCRIPTS_DIR/../infra-modules
 
-LOGGING_SERVER_CODE_DIR=$INFRA_MODULES_HOME_DIR/centralized-logging-efk
-NGINX_PROXY_CODE_DIR=$INFRA_MODULES_HOME_DIR/centralized-logging-efk
+export LOGGING_SERVER_CODE_DIR=$INFRA_MODULES_HOME_DIR/centralized-logging-efk
+export NGINX_PROXY_CODE_DIR=$INFRA_MODULES_HOME_DIR/centralized-logging-efk
 
 
-MACHINE_NAME_LOGGING_SERVER=infra-logging-server
-MACHINE_NAME_CI=infra-ci-server
+export MACHINE_NAME_LOGGING_SERVER=infra-logging-server
+export MACHINE_NAME_CI=infra-ci-server
 
