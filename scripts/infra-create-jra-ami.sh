81ff938c60a8a30d3463a65b67fe74bd969e4cfe
Joe Rice <joericearchitect@gmail.com>
2017-01-02 16:22:55 -0500

    made major progress on automating aws and docker swarm buildout.  Now creates environment and configures docker swarm.  More work to do

---
+++ scripts/infra-create-jra-ami.sh
@@ -0,0 +1,20 @@
+# *********************************************************************************
+# Description: provision a new docker host machine in AWS to be used for the wordpress blog
+# Author:      Joe Rice
+# Created:     10/14/2016
+#
+# Notes:
+#    Script assumes that the following environment variables are set
+#       export AWS_ACCESS_KEY=<Secret>
+#       export AWS_SECRET_KEY=<Super_Top_Secret>
+# *********************************************************************************
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+source $DIR/setenv.sh
+
+cd $INFRA_ENVIRONMENTS_HOME_DIR/packer/jra-ami-docker-ubuntu
+
+pwd
+
+packer build docker-jra-image.json
--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,6 +1,7 @@
 export SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 export INFRA_MODULES_HOME_DIR=$SCRIPTS_DIR/../infra-modules
+export INFRA_ENVIRONMENTS_HOME_DIR=$SCRIPTS_DIR/../environments
 
 export LOGGING_SERVER_CODE_DIR=$INFRA_MODULES_HOME_DIR/centralized-logging-efk
 export NGINX_PROXY_CODE_DIR=$INFRA_MODULES_HOME_DIR/centralized-logging-efk
@@ -9,3 +10,4 @@ export NGINX_PROXY_CODE_DIR=$INFRA_MODULES_HOME_DIR/centralized-logging-efk
 export MACHINE_NAME_LOGGING_SERVER=infra-logging-server
 export MACHINE_NAME_CI=infra-ci-server
 
+export JRA_BUILD_PRIVATE_KEY_FILE='/home/vagrant/share/aws-keys/jra-build.pem'
